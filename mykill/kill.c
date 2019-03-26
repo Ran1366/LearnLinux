07f13288528396d7f1e537a5fc60d75b783cc4a0
Ran1366 <m13669286873_1@163.com>
2019-03-26 22:57:37 +0800

    调用系统函数kill实现的kill命令

---
+++ mykill/kill.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+  if(argc < 3)
+  {
+    printf("./kill signal pid\n");
+    exit(1);
+  }
+  if((kill((pid_t)atoi(argv[2]), atoi(argv[1]))) < 0)
+  {
+    perror("kill");
+    exit(1);
+  }
+  return 0;
+}
